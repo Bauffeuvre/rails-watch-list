fb4d024150b92e655f5f948d8e81f83486aa1b28
baptiste auffeuvre <baptiste.auffeuvre@gmail.com>
2021-05-13 07:16:31 -0400

    Added the validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,8 @@
 class Movie < ApplicationRecord
-  has_many :bookmark
+  has_many :bookmarks
+  
+  validates :title, presence: true
+  validates :title, uniqueness: true
+  validates :overview, presence: true
+  
 end
